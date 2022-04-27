23a2959403d5d0e01220168c54e34389d437be4e
Никита Водяков <nikita.vodyakov@gmail.com>
2022-04-27 22:17:24 +0300

    Добавлены urls и view-функции

--- YaTube_Project/posts/views.py
+++ YaTube_Project/posts/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 
-def index(request):    
+
+def index(request):
     return HttpResponse('Главная страница')
+
 
 def all_groups(request):
     return HttpResponse('Все группы YaTube')
 
+
 def group_posts(request, slug):
-    return HttpResponse('Группа YaTube: {slug}')
+    return HttpResponse(f'Группа YaTube: {slug}')
