c354895ddb1a84dac36738575be6a62ff5f7dfc4
Никита Водяков <nikita.vodyakov@gmail.com>
2022-05-20 12:17:19 +0300

    готово для ревью

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Post, Group
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
@@ -12,10 +13,11 @@ class PostAdmin(admin.ModelAdmin):
     list_editable = ('group',)
     search_fields = ('text',)
     list_filter = ('pub_date',)
-    empty_value_display = '-пусто-' 
+    empty_value_display = '-пусто-'
 
 
 admin.site.register(Post, PostAdmin)
+
 
 class GroupAdmin(admin.ModelAdmin):
     list_display = (
@@ -23,4 +25,6 @@ class GroupAdmin(admin.ModelAdmin):
         'slug',
         'description'
     )
-admin.site.register(Group, GroupAdmin) 
+
+
+admin.site.register(Group, GroupAdmin)
---
+++ yatube/posts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def all_groups(request):
+    return render(request, 'posts/all_groups.html')
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
